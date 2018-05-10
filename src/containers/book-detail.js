761e498d2f573fd92aac983ad2dfe45fb0104e77
jeweltot <mynamewys@gmail.com>
2018-05-11 08:57:53 +0900

    Initial commit

---
+++ src/containers/book-detail.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+class BookDetail extends Component {
+    render () {
+        if (!this.props.book)
+        {
+            return <div>책을 선택하세요!</div>
+        }
+
+        return (
+            <div>
+                <div>{this.props.book.title}</div>
+                <div>{this.props.book.page}</div>
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state)
+{
+    return {
+        book : state.activeBook
+    }
+}
+
+export default connect(mapStateToProps)(BookDetail);
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        {title : 'javascript', page : 100},
+        {title : 'java', page : 200},
+        {title : 'react', page : 300},
+        {title : 'php', page : 400}
+    ]
+}
